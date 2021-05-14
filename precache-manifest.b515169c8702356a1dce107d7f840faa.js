8ab59517e81e4b6421240577f2b2158da3d072ed
Subham <paras123hzb@gmail.com>
2021-05-14 21:34:42 +0530

    Updates

---
+++ precache-manifest.b515169c8702356a1dce107d7f840faa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dde44b0b8ed47befd725c6116d445f67",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed5874af5ddd9d27c8fd",
+    "url": "/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "97534c304db07598cef5",
+    "url": "/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "ed5874af5ddd9d27c8fd",
+    "url": "/static/js/2.ad8e3933.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/static/js/2.ad8e3933.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97534c304db07598cef5",
+    "url": "/static/js/main.1b5ed7a9.chunk.js"
+  },
+  {
+    "revision": "3ff5d6fb19a2b7a4acf2",
+    "url": "/static/js/runtime-main.8b39ddf8.js"
+  }
+]);
